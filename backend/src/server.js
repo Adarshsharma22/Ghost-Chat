337035ba1dc776870b134ef116a6c596d3b8360f
Adarshsharma22 <adarshsharma6222@gmail.com>
2026-06-13 20:37:27 +0530

    Implement user and message models, database connection, and update server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,26 @@
 import express from "express";
+import "dotenv/config"
+import cors from "cors";
+import connectDB from "./lib/db.js";
+import User from "./models/user.model.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.use(express.json());
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true
+}));
+app.use(clerkMiddleware());
+
+app.get("/working", (req, res) => {
+    res.status(200).json({ message: true });
+});
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running on port: ", PORT);
+});
